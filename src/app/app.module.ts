991baaa336d742aac52923db76f769884180ec72
erickecp <43622490+erickecp@users.noreply.github.com>
2023-05-30 08:42:51 -0600

    xd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
@@ -40,8 +40,9 @@ import { MatCardModule } from '@angular/material/card';
     MatIconModule,
     MatSnackBarModule,
     MatCardModule,
-    ReactiveFormsModule
-   ],
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
